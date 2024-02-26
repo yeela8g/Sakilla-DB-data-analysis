51903b365afdd3d119b5df8fccc65d4ffd01eefa
Ye'ela Granot <118124478+yeela8g@users.noreply.github.com>
2024-02-26 15:10:26 +0200

    Delete q5.sql

--- q5.sql
+++
@@ -1,7 +0,0 @@
-# List the average movie length for every film category.
-
-SELECT c.name ,AVG(f.length) as avg_length
-FROM film AS f ,film_category as fc , category AS c
-where f.film_id = fc.film_id
-AND fc.category_id = c.category_id
-group by c.name;
