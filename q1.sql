6d20e83f07735073aec2d49650fadf2bb29a2d3a
Ye'ela Granot <118124478+yeela8g@users.noreply.github.com>
2024-02-26 15:09:43 +0200

    Delete q1.sql

--- q1.sql
+++
@@ -1,5 +0,0 @@
-# Find all category names that contain the letter ‘a’ only once.
-SELECT name
-FROM category
-#beggining,(everything that isn't a)* , than a , (everything that isn't a)* , end. 
-WHERE REGEXP_LIKE(name,'^[^a]*a[^a]*$');
